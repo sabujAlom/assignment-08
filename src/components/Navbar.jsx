001bc56b541699a466db8f4711e1e1eee1a355fc
sabujAlom <sabujalom18@gmail.com>
2026-07-13 18:53:23 +0600

    logo sizing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export default  function Navbar() {
 
     return (
         <div className="navbar shadow-lg bg-[#1d1b20] fixed z-50 p-[4px]">
-            <div className="navbar-start">
+            <div className="navbar-start p-1">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -50,7 +50,9 @@ export default  function Navbar() {
 
                     </ul>
                 </div>
-                <Link href='/home'> <Image src={logo} width={80} height={80} alt='Logo' /> </Link>
+                 <div >
+                    <Link href='/home'> <Image src={logo} width={70} height={70} alt='Logo' /> </Link>
+                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 gap-4">
