f4cc327cfbf5a0e8a4cf7f3c828602d0c146ff1e
sabujAlom <sabujalom18@gmail.com>
2026-07-14 17:02:45 +0600

    butter auth implement

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -11,7 +11,7 @@ const db = client.db("ReedBook");
 export const auth = betterAuth({
     trustedOrigins: [
         "http://localhost:3000",
-        // "https://neyamul-assignment-8.vercel.app",
+        // "https:assignment-8.vercel.app",
     ],
 
     database: mongodbAdapter(db, {
