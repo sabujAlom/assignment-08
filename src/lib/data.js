84c65ae9a0d79e46f6455caf752235eb2cb4c25b
sabujAlom <sabujalom18@gmail.com>
2026-07-14 17:52:51 +0600

    fetch updated

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,19 +1,19 @@
 export async function getBooks () {
-    const res = await fetch('https://neyamul-assignment-8.vercel.app/books.json')
+    const res = await fetch('https://assignment-08-99i4.vercel.app/books.json')
     const data = res.json()
     return data
 }
 
 
 export async function features () {
-    const res = await fetch('https://neyamul-assignment-8.vercel.app/feature.json')
+    const res = await fetch('https://assignment-08-99i4.vercel.app/feature.json')
     const data = res.json()
     return data
 }
 
 
 export async function review () {
-    const res = await fetch('https://neyamul-assignment-8.vercel.app/review.json')
+    const res = await fetch('https://assignment-08-99i4.vercel.app/review.json')
     const data = res.json()
     return data
 }
