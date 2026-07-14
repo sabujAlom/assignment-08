6f1b15e90725787c42bec4e001a3c515a4af767b
sabujAlom <sabujalom18@gmail.com>
2026-07-14 17:32:48 +0600

    updated text

--- src/app/(main)/details/[id]/page.jsx
+++ src/app/(main)/details/[id]/page.jsx
@@ -1,41 +1,19 @@
 // 'use client'
-import DetailsCom from '@/components/DetailsCom';
-import { authClient } from '@/lib/auth-client';
+import DetailsCom from "@/components/DetailsCom";
+import { authClient } from "@/lib/auth-client";
 // import { authClient } from '@/lib/auth-client';
-import { getBooks } from '@/lib/data';
+import { getBooks } from "@/lib/data";
 // import Image from 'next/image';
-import React from 'react'
+import React from "react";
 
-export default async function Details({params}) {
+export default async function Details({ params }) {
   // const {data: session} = authClient.useSession()
   // console.log(session);
-  
-  const book = await getBooks()
-  const { id } = await params
-  const detail = book.find(b => b.id == id)
+
+  const book = await getBooks();
+  const { id } = await params;
+  const detail = book.find((b) => b.id == id);
   console.log(detail);
 
-
-  return (
-    <DetailsCom detail={detail}></DetailsCom>
-    // <div className="hero bg-base-300 min-h-screen pt-30 px-10 lg:px-0">
-    //   <div className="hero-content bg-base-100 flex-col lg:flex-row gap-10 container mx-auto border w-fit p-20 rounded-2xl ">
-    //     <Image
-    //       width={200}
-    //       height={400}
-    //       alt={detail.title}
-    //       src={detail.image_url}
-    //       className="max-w-sm rounded-lg shadow-2xl"
-    //     />
-    //     <div>
-    //       <h1 className="text-5xl font-bold">{detail.title}</h1>
-    //       <p className="py-2 text-xl">{detail.description}</p>
-    //       <p className='text-xl'>Writter name : {detail.author}</p>
-    //       <p> Category : {detail.category}</p>
-    //       <p>Available Quantity : {detail.available_quantity}</p>
-    //       <button className="btn bg-gray-500 my-3">Borrow this Book</button>
-    //     </div>
-    //   </div>
-    // </div>
-  )
+  return <DetailsCom detail={detail}></DetailsCom>;
 }
--- src/app/all-books/page.jsx
+++ src/app/all-books/page.jsx
@@ -70,15 +70,14 @@ export default function AllBooks() {
                   fill="none"
                   stroke="currentColor"
                 >
-                  {/* <rect width="20" height="16" x="2" y="4" rx="2"></rect> */}
-                  {/* <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path> */}
+                  
                 </g>
               </svg>
               <input type="text" onChange={search} placeholder="Search by title" required />
             </label>
-            {/* <div className="validator-hint hidden">Enter valid email address</div> */}
+           
           </div>
-          {/* <button className="btn button btn-neutral join-item">Search</button> */}
+          
         </div>
       </div>
 
